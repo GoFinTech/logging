c8cbec3fa429d8da96436993e453b676307f30f4
Antanas Kompanas <a.kompanas@manounija.lt>
2018-09-09 14:14:58 +0300

    Public release.

---
+++ src/Logger.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace GoFinTech\Logging;
+
+class Logger
+{
+    public static function get($parent)
+    {
+        return new Logger();
+    }
+
+    public function info(string $message)
+    {
+        echo "$message\n";
+    }
+
+    public function error(string $message, \Exception $ex = null)
+    {
+        $msg = "ERROR $message";
+        if (is_null($ex))
+            $msg = "ERROR $message\n";
+        else
+            $msg = "ERROR $message [" . $this->formatException($ex) . "]\n"
+                . $ex->getTraceAsString() . "\n";
+
+        fwrite(STDERR, $msg);
+    }
+
+    public function formatException(\Exception $ex): string
+    {
+        $className = get_class($ex);
+        return "{$ex->getFile()}:{$ex->getLine()} $className: {$ex->getMessage()}";
+    }
+}
